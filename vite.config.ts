370e29fddc1c4e4782dd119f32034a14acc1d84e
Anthony <anthonywlynn2002@gmail.com>
2025-10-26 00:39:56 -0400

    fix: attempt to fix page not working in gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
