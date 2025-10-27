64fe22dc36a4bf42465c74fac62b2a290b24cc94
Anthony <anthonywlynn2002@gmail.com>
2025-10-27 01:32:26 -0400

    build: add support for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
