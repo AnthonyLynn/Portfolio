281aaffbc37c7e387cf24bb3545d9f06f264098a
Anthony <anthonywlynn2002@gmail.com>
2025-10-31 14:46:49 -0400

    fix: link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export const Footer: React.FC<FooterProps> = ({}) => {
     <footer className="w-full bg-base-primary flex justify-center">
       <div className="flex justify-between w-full md:w-[640px] p-4 px-3 sm:px-4 md:px-0 items-center">
         <p className="font-medium text-xs text-text-secondary">
-          &copy; {currentYear} anthonywlynn.com{" "}
+          &copy; {currentYear} anthony-lynn.com
         </p>
         <nav className="flex gap-4">
           <a href="https://www.linkedin.com/in/anthony-w-lynn" target="_blank">
