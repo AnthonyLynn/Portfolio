920656b8fd023a0a3843c7e269f5329121b1f16c
Anthony <anthonywlynn2002@gmail.com>
2026-01-06 20:02:38 -0500

    feat: setup backend

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`App is listening to port ${PORT}...`);
+});
